2ae65df1e2cc8f63d9ab4fa831d9ae1e7b967733
alexandru <alexandru@Macs-MacBook-Air.local>
2017-02-13 12:42:27 +0200

    Finished project "Quotes App" from Udemy Ionic 2

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import {Quote} from "../../data/quote.interface";
 import {QuotesService} from "../../services/quotes";
+import {ModalController, MenuController} from "ionic-angular";
+import {QuotePage} from "../quote/quote";
+import {SettingsService} from "../../services/settings";
 
 
 @Component({
@@ -10,9 +13,45 @@ import {QuotesService} from "../../services/quotes";
 export class FavoritesPage {
   quotes: Quote[];
 
-  constructor(private quotesService: QuotesService){}
+  constructor(private quotesService: QuotesService,
+              private modalCtrl: ModalController,
+              private menuCtrl: MenuController,
+              private settingsService: SettingsService) {
+  }
 
-  ionViewWillEnter(){
+  ionViewWillEnter() {
     this.quotes = this.quotesService.getFavoriteQuotes();
+  }
+
+  onViewQuote(quote: Quote) {
+    const modal = this.modalCtrl.create(QuotePage, quote);
+    modal.present();
+    modal.onDidDismiss((remove: boolean) => {
+      if (remove) {
+        this.onRemoveFromFavorites(quote)
+      }
+    });
+  }
+
+  onRemoveFromFavorites(quote: Quote){
+    this.quotesService.removeQuoteFromFavorites(quote);
+    this.quotes = this.quotesService.getFavoriteQuotes();
+
+    const position = this.quotes.findIndex((quoteEl: Quote) => {
+    return quoteEl.id == quote.id;  });
+    this.quotes.splice(position, 1);
+
+  }
+
+  onOpenMenu(){
+    this.menuCtrl.open();
+  }
+
+  getBackground(){
+    return this.settingsService.isAltBackground()? 'altQuoteBackground' : 'quoteBackground';
+  }
+
+  isAltBackground(){
+    return this.settingsService.isAltBackground()
   }
 }
