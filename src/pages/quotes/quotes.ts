dfcefa4228eb08f79afd62e2699c3b240bfc43a4
alexandru <alexandru@Macs-MacBook-Air.local>
2017-02-13 09:49:10 +0200

    Another day another dollar

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import { NavParams } from "ionic-angular";
+import {NavParams, AlertController} from "ionic-angular";
 import { Quote } from '../../data/quote.interface';
+import {QuotesService} from "../../services/quotes";
 
 
 @Component({
@@ -10,7 +11,9 @@ import { Quote } from '../../data/quote.interface';
 export class QuotesPage implements OnInit {
 
   quoteGroup: {category: string, quotes: Quote[], icon: string};
-  constructor(private navParams: NavParams){}
+  constructor(private navParams: NavParams,
+              private alertCtrl: AlertController,
+              private quotesService: QuotesService){}
 
   // ionViewDidLoad(){
   //   this.quoteGroup = this.navParams.data;
@@ -18,6 +21,31 @@ export class QuotesPage implements OnInit {
   // }
   ngOnInit(){
     this.quoteGroup = this.navParams.data;
+  }
+
+  onAddToFavorite(selectedQuote: Quote){
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons:[{
+        text: 'Yes, go ahead',
+        handler: () => {
+          this.quotesService.addQuoteToFavorites(selectedQuote);
+          console.log('Ok');
+        }
+      },
+        {
+          text: 'No, I changed my mind',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelled!');
+          }
+        }
+
+      ]
+    });
+    alert.present();
   }
 
 }
